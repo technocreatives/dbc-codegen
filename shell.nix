0401810862ce933b23c759631d18365acbd3f20d
Pascal Hertleif <pascal@technocreatives.com>
2021-03-18 20:56:35 +0100

    Fix nix env

--- shell.nix
+++ shell.nix
@@ -7,8 +7,13 @@ pkgs.mkShell {
     nodejs-12_x
     python37 # for node-gyp
     can-utils
+    clang
+    llvmPackages.libclang.lib
 
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
+
+  # Needed so bindgen can find libclang.so
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 }
