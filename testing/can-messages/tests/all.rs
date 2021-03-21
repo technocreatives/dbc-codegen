c32158e35b7e8fa9e239366bfdf7a40e5d1c5abe
Pascal Hertleif <pascal@technocreatives.com>
2021-03-21 14:47:45 +0100

    Add tests for debug impl, enable range_check

--- testing/can-messages/tests/all.rs
+++ testing/can-messages/tests/all.rs
@@ -1,11 +1,11 @@
 #![allow(clippy::float_cmp)]
 
-use can_messages as messages;
+use can_messages::{Amet, Bar, CanError, Foo};
 
 #[test]
 #[cfg(feature = "range_checked")]
 fn check_range_value_error() {
-    let result = messages::Bar::new(1, 2.0, 3, 4);
+    let result = Bar::new(1, 2.0, 3, 4, true);
     assert!(matches!(
         result,
         Err(CanError::ParameterOutOfRange { message_id: 512 })
@@ -15,30 +15,49 @@ fn check_range_value_error() {
 #[test]
 #[cfg(feature = "range_checked")]
 fn check_range_value_valid() {
-    let result = messages::Bar::new(1, 2.0, 3, 3);
+    let result = Bar::new(1, 2.0, 3, 3, true);
     assert!(result.is_ok());
 }
 
 #[test]
 fn pack_unpack_message() {
-    let result = messages::Foo::new(63.99899, 10.0).unwrap();
+    let result = Foo::new(63.9990234375, 10.0).unwrap();
     assert_eq!(result.voltage_raw(), 63.99899);
     assert_eq!(result.current_raw(), 10.0);
 }
 
 #[test]
 fn pack_unpack_message_negative() {
-    let result = messages::Foo::new(0.000976562, -3.0 * 0.0625).unwrap();
+    let result = Foo::new(0.000976562, -3.0 * 0.0625).unwrap();
     assert_eq!(result.voltage_raw(), 0.000976562);
     assert_eq!(result.current_raw(), -3.0 * 0.0625);
 }
 
 #[test]
 fn pack_unpack_message2() {
-    let result = messages::Amet::new(1, 0.39, 3, 3, true).unwrap();
+    let result = Amet::new(1, 0.39, 3, 3, true).unwrap();
     assert_eq!(result.one_raw(), 1);
     assert_eq!(result.two_raw(), 0.39);
     assert_eq!(result.three_raw(), 3);
     assert_eq!(result.four_raw(), 3);
     assert_eq!(result.five_raw(), true);
+}
+
+#[test]
+#[cfg(feature = "debug")]
+fn debug_impl() {
+    let result = Bar::new(1, 2.0, 3, 3, true).unwrap();
+    let dbg = format!("{:?}", result);
+    assert_eq!(&dbg, "Bar([5, 94, 0, 64, 0, 0, 0, 0])");
+}
+
+#[test]
+#[cfg(feature = "debug")]
+fn debug_alternative_impl() {
+    let result = Bar::new(1, 2.0, 3, 3, true).unwrap();
+    let dbg = format!("{:#?}", result);
+    assert_eq!(
+        &dbg,
+        "Bar {\n    one: 1,\n    two: 1.9499999,\n    three: Onest,\n    four: Onest,\n    xtype: X1on,\n}"
+    );
 }
