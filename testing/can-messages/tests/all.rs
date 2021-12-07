0114657e4de484d891a24bcda38c7df5fd65a4b4
Manuel Valenzuela <manuel.valenzuela@moixa.com>
2021-12-07 14:16:54 +0000

    Approx library not needed in the end

--- testing/can-messages/tests/all.rs
+++ testing/can-messages/tests/all.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::float_cmp)]
 
-use approx::assert_ulps_eq;
 use can_messages::{
     Amet, Bar, BarThree, CanError, Foo, FooInexact, MultiplexTest, MultiplexTestMultiplexor,
     MultiplexTestMultiplexorM0,
@@ -45,7 +44,7 @@ fn pack_unpack_message() {
 #[test]
 fn pack_unpack_message_inexact_scale() {
     let result = FooInexact::new(0.08, -0.035).unwrap();
-    assert_ulps_eq!(result.voltage_raw(), 0.08);
+    assert_eq!(result.voltage_raw(), 0.08);
     assert_eq!(result.current_raw(), -0.035);
 }
 
