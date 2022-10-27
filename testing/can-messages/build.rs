b65d976c84b4f31a78fd8c513ada47e0d8b6be13
Zhao, Gang <gang.zhao.42@gmail.com>
2022-10-27 22:18:23 +0800

    Format generated messages.rs in build.rs
    So we won't need to format it manually.

--- testing/can-messages/build.rs
+++ testing/can-messages/build.rs
@@ -1,15 +1,29 @@
 use anyhow::Result;
 use std::{
     fs::{self, File},
-    io::BufWriter,
+    io::{BufWriter, Write},
+    process::Command,
 };
 
 fn main() -> Result<()> {
+    let out_file = "src/messages.rs";
     let dbc_file = fs::read("../dbc-examples/example.dbc")?;
-    let mut out = BufWriter::new(File::create("src/messages.rs")?);
+    let mut out = BufWriter::new(File::create(out_file)?);
     println!("cargo:rerun-if-changed=../dbc-examples/example.dbc");
     println!("cargo:rerun-if-changed=../../src");
 
     dbc_codegen::codegen("example.dbc", &dbc_file, &mut out, true)?;
+
+    out.flush()?;
+
+    if cfg!(target_os = "linux") {
+        Command::new("rustfmt")
+            .arg("--edition")
+            .arg("2018")
+            .arg(out_file)
+            .output()
+            .expect("failed to execute rustfmt");
+    }
+
     Ok(())
 }
